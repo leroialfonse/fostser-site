5fe1001c7266910b694346f137d0e00fb256cd0d
Brandon White <brandonkwhite3@gmail.com>
2023-05-09 13:28:47 -0400

    Building out ForeverHome

---
+++ ForeverHome/src/App.jsx
@@ -0,0 +1,23 @@
+// import React, { useState } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import About from './components/About/About'
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+    </div>
+  )
+}
+
+export default App
